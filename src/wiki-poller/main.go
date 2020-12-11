9fffe46d2d2235ee67887747c5aa03dce96e12e5
phewitt-uw <phewitt@uw.co.uk>
2020-12-11 09:12:00 +0000

    fixed some issues with capitalization in okta

--- src/wiki-poller/main.go
+++ src/wiki-poller/main.go
@@ -40,12 +40,16 @@ func main() {
 		os.Exit(1)
 	}
 	if len(wikiapi.Oktaprovider) < 1 {
-		fmt.Println("env variable wiki.oktaprovider not set")
+		fmt.Println("env variable oktaprovider not set")
 		os.Exit(1)
 	}
 
 	// get duration
 	WikiIntervalInt, _ := strconv.Atoi(os.Getenv("WikiInterval"))
+	if WikiIntervalInt == 0 {
+		fmt.Println("env variable WikiInterval not set")
+		os.Exit(1)
+	}
 	WikiIntervalDuration := time.Duration(WikiIntervalInt)
 	//run ticker
 	doEvery(WikiIntervalDuration*time.Second, mainprog)
@@ -74,33 +78,35 @@ func mainprog(t time.Time) {
 		// parse list of users and get details
 		for _, userdetails := range oktauserlistout {
 			// check if user exists
-			usercheck, _ := wikiapi.DoesUserExist(userdetails.Profile.Email)
+
+			useremail := strings.ToLower(userdetails.Profile.Email)
+
+			usercheck, _ := wikiapi.DoesUserExist(useremail)
 			if usercheck {
-				//fmt.Println("--", userdetails.Profile.Email, "from okta ALREADY EXISTS in wiki")
 				// add user to wiki group based on okta group
-				if !strings.Contains(wikigroupuserjson, userdetails.Profile.Email) {
-					if wikiapi.AddUserNameToGroupName(wikigroup, userdetails.Profile.Email) {
-						fmt.Println("added user", userdetails.Profile.Email, "to wiki group -", wikigroup)
+				if !strings.Contains(wikigroupuserjson, useremail) {
+					if wikiapi.AddUserNameToGroupName(wikigroup, useremail) {
+						fmt.Println("1 added user", useremail, "to wiki group -", wikigroup)
 					} else {
-						fmt.Println("ERROR adding user", userdetails.Profile.Email, "to", wikigroup)
+						fmt.Println("ERROR adding user", useremail, "to", wikigroup)
 					}
 					//} else {
-					//	fmt.Println("--", userdetails.Profile.Email, "ALREADY EXISTS in wiki group -", wikigroup)
+					//	fmt.Println("--", useremail, "ALREADY EXISTS in wiki group -", wikigroup)
 				}
 			} else {
-				//fmt.Println("user from okta MISSING in wiki", userdetails.Profile.Email, usercheck)
+				//fmt.Println("user from okta MISSING in wiki", useremail, usercheck)
 				// if user is missing create user with okta link and default group
-				createuserresult, slug := wikiapi.CreateNewUser(userdetails.Profile.Email, ""+userdetails.Profile.FirstName+" "+userdetails.Profile.LastName+"", "placeholderpas$wordb3causeOKTA3213", "3")
+				createuserresult, slug := wikiapi.CreateNewUser(useremail, ""+userdetails.Profile.FirstName+" "+userdetails.Profile.LastName+"", "placeholderpas$wordb3causeOKTA3213", "3")
 				if createuserresult {
-					fmt.Println("Created user in wiki", userdetails.Profile.Email)
+					fmt.Println("Created user in wiki", useremail)
 				} else {
-					fmt.Println("ERROR creating user in wiki", userdetails.Profile.Email, slug)
+					fmt.Println("ERROR creating user in wiki", useremail, slug)
 				}
 				// now add user to wiki group based on okta group
-				if wikiapi.AddUserNameToGroupName(wikigroup, userdetails.Profile.Email) {
-					fmt.Println("added user", userdetails.Profile.Email, "to wiki group -", wikigroup)
+				if wikiapi.AddUserNameToGroupName(wikigroup, useremail) {
+					fmt.Println("2 added user", useremail, "to wiki group -", wikigroup)
 				} else {
-					fmt.Println("ERROR adding user", userdetails.Profile.Email, "to wiki group -", wikigroup)
+					fmt.Println("ERROR adding user", useremail, "to wiki group -", wikigroup)
 				}
 			}
 		}
@@ -109,7 +115,7 @@ func mainprog(t time.Time) {
 		json.Unmarshal([]byte(wikigroupuserjson), &wikiuserlistout)
 		// check wiki user list against okta group and remove users not in okta group
 		for _, wikiuserdetails := range wikiuserlistout.Groups.Single.Users {
-			if !strings.Contains(oktauserlist, wikiuserdetails.Email) {
+			if !strings.Contains(strings.ToLower(oktauserlist), strings.ToLower(wikiuserdetails.Email)) {
 				// remove user from wiki if condition met
 				if wikiapi.RemoveUserNameFromGroupName(wikigroup, wikiuserdetails.Email) {
 					fmt.Println("user removed from wiki group", wikiuserdetails.Email, "-", wikigroup)
@@ -119,4 +125,5 @@ func mainprog(t time.Time) {
 			}
 		}
 	}
+	fmt.Println("Completed poll:", time.Now())
 }
